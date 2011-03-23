7e42daa0cba8800e98e7445770870b688a226edd
miyauchi <miyauchi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-23 12:16:16 +0000

    first commit
    git-svn-id: https://plugins.svn.wordpress.org/url2link/trunk@363671 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,60 @@
+=== url2link ===
+Contributors: miyauchi
+Donate link: http://firegoby.theta.ne.jp/
+Tags: url, oembed, blackbird, visual editor, post
+Requires at least: 3.0
+Tested up to: 3.1
+Stable tag: 0.2.0
+
+Insert link to post with only the URL as the input.
+
+== Description ==
+
+Insert link with title and summary only input URL.
+
+If you input URL on visual editor.
+This plugin will fetch title and summary from URL.
+Then changed to link with summary automatically.
+
+
+= Support =
+
+* [Plugin Homepage](http://firegoby.theta.ne.jp/wp/url2link)
+* [Support](http://wordpress.org/tags/url2link)
+
+= Some features: =
+
+* You can embed link easily.
+* You can edit CSS in your theme directory.
+(url2link.css copy to your theme directory then edit it!)
+
+
+== Installation ==
+
+* A plug-in installation screen is displayed on the WordPress admin panel.
+* It installs it in `wp-content/plugins`.
+* The plug-in is made effective.
+
+
+== Screenshots ==
+
+1. Paste a URL in visual editor.
+1. Embed link with title and summary.
+
+== Changelog ==
+
+= 0.2.0 =
+* The first release.
+
+= 0.1 =
+* The development edition release.
+
+== Credits ==
+
+This plug-in is not guaranteed though the user of WordPress can freely use this plug-in free of charge regardless of the purpose.
+The author must acknowledge the thing that the operation guarantee and the support in this plug-in use are not done at all beforehand.
+
+== Contact ==
+
+* email to miya[at]theta.ne.jp
+* twitter @miya0001
---
+++ url2link.php
@@ -0,0 +1,157 @@
+<?php
+/*
+Plugin Name: url2link
+Plugin URI: http://firegoby.theta.ne.jp/wp/url2link
+Description: convert url to link.
+Author: Takayuki Miyauchi (THETA NETWORKS Co,.Ltd)
+Version: 0.2.0
+Author URI: http://firegoby.theta.ne.jp/
+*/
+
+/*
+Copyright (c) 2010 Takayuki Miyauchi (THETA NETWORKS Co,.Ltd).
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+require_once(dirname(__FILE__).'/includes/getSiteSummary.php');
+
+new url2link();
+
+class url2link {
+
+    private $length = 150;
+    private $meta_id = '_url2link_';
+    private $debug = false;
+
+    function __construct()
+    {
+        if (defined('WP_DEBUG') && WP_DEBUG === true) {
+            $this->debug = true;
+        }
+        wp_embed_register_handler(
+            'url2link',
+            '#https?://.+#i',
+            array(&$this, 'handler'),
+            9999
+        );
+        add_action(
+            'wp_head',
+            array(&$this, 'loadCSS')
+        );
+        add_shortcode('url2link', array(&$this, 'shortcode'));
+        add_action( 'save_post', array(&$this, 'delete_oembed_caches') );
+    }
+
+    public function handler($m, $attr, $url, $rattr)
+    {
+        return '[url2link url="'.$url.'"]';
+    }
+
+    private function getCache($id, $key)
+    {
+        if ($this->debug) {
+            return false;
+        } else {
+            return get_post_meta($id, $key, true);
+        }
+    }
+
+    public function shortcode($p)
+    {
+        global $post;
+        $url =  html_entity_decode($p['url']);
+        if ($html = $this->getCache($post->ID, $this->meta_id.$url)) {
+            // since  ver 0.2
+        } elseif ($html = $this->getCache($post->ID, '_'.$url)) {
+            // for ver 0.1
+            delete_post_meta($post->ID, '_'.$url);
+            add_post_meta($post->ID, $this->meta_id.$url, $html);
+        } else {
+            $summary = '';
+            if (isset($p['summary']) && $p['summary']) {
+                $summary = esc_html($p['summary']);
+            } elseif (strpos($url, site_url()) > -1) {
+                $pid = url_to_postid($url);
+                $post = get_post($pid);
+                $link = sprintf('<a href="%s">%s</a>', $url, $post->post_title);
+                $summary = strip_tags($post->post_content);
+            } else {
+                if (isset($p['charset']) && $p['charset']) {
+                    $obj = new getSiteSummary($url, $p['charset']);
+                } else {
+                    $obj = new getSiteSummary($url);
+                }
+                $site = $obj->fetch();
+                if (!$site) {
+                    return ;
+                }
+                $link = sprintf('<a href="%s">%s</a>', $url, $site['title']);
+                $summary = $site['summary'];
+            }
+            if ($summary) {
+                if (isset($p['length']) && is_int($p['length']) && $p['length']) {
+                    $this->length = $p['length'];
+                }
+                $summary = mb_substr($summary, 0, $this->length).'...';
+                $summary = "<div class=\"link_summary\">{$summary}</div>";
+            }
+            $html = sprintf($this->gethtml(), $link, $summary);
+            add_post_meta($post->ID, $this->meta_id.$url, $html);
+        }
+        return $html;
+    }
+
+    private function gethtml()
+    {
+        $html =<<<EOL
+<div class="url2link">
+    <div class="link_title">%s</div>
+    %s
+</div>
+EOL;
+        return $html;
+    }
+
+    public function delete_oembed_caches( $post_ID ) {
+        $post_metas = get_post_custom_keys( $post_ID );
+        if (empty($post_metas))
+            return;
+
+        foreach ($post_metas as $post_meta_key) {
+            if ($this->meta_id == substr( $post_meta_key, 0, strlen($this->meta_id)))
+                delete_post_meta( $post_ID, $post_meta_key );
+        }
+    }
+
+    public function loadCSS()
+    {
+        $style = get_template_directory().'/url2link.css';
+        if (is_file($style)) {
+            $style = get_bloginfo('stylesheet_directory').'/url2link.css';
+        } else {
+            $url = WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__));
+            $style = $url.'/url2link.css';
+        }
+        echo "<!--url2link plugin-->\n";
+        echo '<link rel="stylesheet" type="text/css" media="all" href="'.$style.'">';
+    }
+}
+
+?>
